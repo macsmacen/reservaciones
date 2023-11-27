dcf303046d2206d7fa59671d857fb678d2f1306f
IkerMarcor <99061325+IkerMarcor@users.noreply.github.com>
2023-11-26 20:00:20 -0700

    TDD Implementado
    Se agrego la libreria para hacer los testings y se organizo el archivo 'TestReserva.java'

--- pruebas/TestReserva.java
+++ pruebas/TestReserva.java
@@ -1,28 +1,34 @@
 package pruebas;
 
-import static org.junit.Assert.*;
+import org.junit.*;
 
 import clases.Cliente;
 import clases.Mesa;
 import clases.Reserva;
-import org.junit.Test;
 
 import java.time.LocalDateTime;
 
 public class TestReserva {
+
+    private Cliente cliente;
+    private Mesa mesa;
+    private LocalDateTime fechaHora;
+    private Reserva reserva;
     
+    @Before
+    public void setUp(){
+        cliente = new Cliente("Pepe","6627287584");
+        mesa = new Mesa(1, 4);
+        fechaHora = LocalDateTime.of(2023, 11, 25, 18, 30);
+        reserva = new Reserva(cliente, mesa, fechaHora, 3);
+    }
+
     @Test
     public void testCrearReserva() {
-        Cliente cliente = new Cliente("NombreCliente", "123456789");
-        Mesa mesa = new Mesa(1, 4);
-        LocalDateTime fechaHora = LocalDateTime.of(2023, 11, 25, 18, 30);
-        
-        Reserva reserva = new Reserva(cliente, mesa, fechaHora, 3);
-        
-        assertEquals(cliente, reserva.getCliente());
-        assertEquals(mesa, reserva.getMesa());
-        assertEquals(fechaHora, reserva.getFechaHora());
-        assertEquals(3, reserva.getNumeroComensales());
+        Assert.assertEquals(cliente, reserva.getCliente());
+        Assert.assertEquals(mesa, reserva.getMesa());
+        Assert.assertEquals(fechaHora, reserva.getFechaHora());
+        Assert.assertEquals(3, reserva.getNumeroComensales());
     }
 
     // Otros tests según las funcionalidades de las clases
